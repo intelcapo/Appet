23e86db2fb6863e9b65b059993dc27509b2c4e9d
Diego  Casallas <diehercasvan@gmail.com>
2016-04-02 23:03:13 -0500

    create project

---
+++ app/src/main/java/com/example/diegoh/appet/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.example.diegoh.appet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by DIEGOH on 2/04/2016
+ * Copyright 2016 Sinapsis Soft.
+ */
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
