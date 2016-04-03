4f4e115af768763c173e682515ddc55509faffbe
Diego  Casallas <diehercasvan@gmail.com>
2016-04-02 23:50:18 -0500

    new changes

---
+++ app/src/main/java/Class/AnimationGeneral.java
@@ -0,0 +1,33 @@
+package Class;
+
+/**
+ * Created by DIEGOH on 2/04/2016
+ * Copyright 2016 Sinapsis Soft.
+ */
+public  class AnimationGeneral {
+    private static AnimationGeneral animations;
+
+    public AnimationGeneral(){
+
+        animations=null;
+    }
+
+    public static  void selectAnimation(int iSelect){
+        switch(iSelect){
+
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/example/diegoh/appet/SplashScreen.java
+++ app/src/main/java/com/example/diegoh/appet/SplashScreen.java
@@ -1,16 +1,29 @@
 package com.example.diegoh.appet;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by DIEGOH on 2/04/2016
  * Copyright 2016 Sinapsis Soft.
  */
-public class SplashScreen extends Activity {
-    @Override
+public class SplashScreen extends Activity implements View.OnClickListener {
+    private ImageView imageView;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        imageView=(ImageView)findViewById(R.id.imgLogo);
+        imageView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
